da7e26dd76fb61bb983748ee3af937d32f900614
Gustavo Baranda <baranda.gustavo@gmail.com>
2023-01-31 23:20:34 -0300

    contact desing maps, contact info

--- src/Components/Footer/index.js
+++ src/Components/Footer/index.js
@@ -1,53 +1,53 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import logofooter from "../../assets/logofooter.svg";
+import React from "react"; 
+import { Link } from "react-router-dom"; 
+import logofooter from "../../assets/logofooter.svg"; 
+ 
+const Footer = () => { 
+  return ( 
+    <footer className="footer__container"> 
+      <div className="footer_container--logo"> 
+        <Link to="/"> 
+          <img src={logofooter} alt="" /> 
+        </Link> 
+      </div> 
+      <div className="footer_container--location"> 
+        <p> 
+          Beltrán 1290, local 5 5501 Godoy Cruz, Provincia de Mendoza, Argentina 
+        </p> 
+        <p> 
+          +54 261 594-5894 -{" "} 
+          <a 
+            className="footer__container--location_mail" 
+            href="mailto:lasverde.tiendasaludable@gmail.com" 
+            target="blank" 
+          > 
+            lasverde.tiendasaludable@gmail.com 
+          </a> 
+        </p> 
+      </div> 
+      <div className="footer__contariner--copyright"> 
+        <p>© Copyright Las Verde, todos los derechos reservados.</p> 
+        <p> 
+          Powered by{" "} 
+          <a 
+            href="https://www.linkedin.com/in/gustavobaranda/" 
+            target="_blank" 
+            rel="noopener noreferrer" 
+          > 
+            Gustavo Baranda 
+          </a>{" "} 
+          &{" "} 
+          <a 
+            href="https://www.linkedin.com/in/melisa-caputo/" 
+            target="_blank" 
+            rel="noopener noreferrer" 
+          > 
+            Melisa Caputo 
+          </a> 
+        </p> 
+      </div> 
+    </footer> 
+  ); 
+}; 
 
-const Footer = () => {
-  return (
-    <footer className="footer__container">
-      <div className="footer_container--logo">
-        <Link to="/">
-          <img src={logofooter} alt="" />
-        </Link>
-      </div>
-      <div className="footer_container--location">
-        <p>
-          Beltrán 1290, local 5 5501 Godoy Cruz, Provincia de Mendoza, Argentina
-        </p>
-        <p>
-          +54 261 594-5894 -{" "}
-          <a
-            className="footer__container--location_mail"
-            href="mailto:lasverde.tiendasaludable@gmail.com"
-            target="blank"
-          >
-            lasverde.tiendasaludable@gmail.com
-          </a>
-        </p>
-      </div>
-      <div className="footer__contariner--copyright">
-        <p>© Copyright Las Verde, todos los derechos reservados.</p>
-        <p>
-          Powered by{" "}
-          <a
-            href="https://www.linkedin.com/in/gustavobaranda/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Gustavo Baranda
-          </a>{" "}
-          &{" "}
-          <a
-            href="https://www.linkedin.com/in/melisa-caputo/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Melisa Caputo
-          </a>
-        </p>
-      </div>
-    </footer>
-  );
-};
-
-export default Footer;
+export default Footer; 
