c0362486211c4497fea20a2483351cbbc56c806e
Gustavo Baranda <baranda.gustavo@gmail.com>
2023-02-04 22:13:03 -0300

    scroll ModalDetail

--- src/Components/Footer/index.js
+++ src/Components/Footer/index.js
@@ -4,28 +4,32 @@ import logofooter from "../../assets/logofooter.svg";
  
 const Footer = () => { 
   return ( 
-    <footer className="footer__container"> 
-      <div className="footer_container--logo"> 
+    <footer className="footerContainer"> 
+      <div className="footerContainer--logo"> 
         <Link to="/"> 
           <img src={logofooter} alt="" /> 
         </Link> 
       </div> 
-      <div className="footer_container--location"> 
+      <div className="footerContainer-location"> 
         <p> 
           Beltrán 1290, local 5 5501 Godoy Cruz, Provincia de Mendoza, Argentina 
         </p> 
-        <p> 
-          +54 261 594-5894 -{" "} 
+        <div className="footerContainer-contact"> 
           <a 
-            className="footer__container--location_mail" 
+            href="tel:+54 261 594-5894"
+          >
+            +54 261 594-5894 - {" "} 
+          </a>
+          <a 
+            // className="footerContainer-location_mail" 
             href="mailto:lasverde.tiendasaludable@gmail.com" 
             target="blank" 
           > 
             lasverde.tiendasaludable@gmail.com 
-          </a> 
-        </p> 
+          </a>
+        </div> 
       </div> 
-      <div className="footer__contariner--copyright"> 
+      <div className="footerContariner-copyright"> 
         <p>© Copyright Las Verde, todos los derechos reservados.</p> 
         <p> 
           Powered by{" "} 
